825dca90b834b487ce30bd1d77d14cbdc3470fff
bolk3 <renald.sonnet59@gmail.com>
2026-04-29 21:31:04 +0200

    Feat (Vue): add the main frame

---
+++ src/com/bibliotheque/Main.java
@@ -0,0 +1,11 @@
+package com.bibliotheque;
+
+import com.bibliotheque.vue.MainFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        MainFrame   frame = new MainFrame();
+
+        frame.setVisible(true);
+    }
+}
